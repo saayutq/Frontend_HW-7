f426cb35c5de3cc804b90e14ebdbc3f5ea10e86d
saayutq <kirushka4real@gmail.com>
2025-01-01 15:36:09 +0300

    UI + details + locations + part of service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { HousingLocationComponent } from './../housing-location/housing-location.component';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
+
 
 @Component({
   selector: 'app-home',
@@ -14,11 +17,16 @@ import { CommonModule } from '@angular/common';
     </form>
   </section>
   <section class="results">
-    <app-housing-location></app-housing-location> 
+    <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location> 
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+housingService: HousingService = inject(HousingService);
 
+constructor() {
+  this.housingLocationList = this.housingService.getAllHousingLocations();
+}
 }
